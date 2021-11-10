a3a39e0869dc64663d9341ba5c1f7745b633fbdd
Dao Viet Anh <vietanh.dao99@gmail.com>
2021-11-11 00:46:27 +0700

    fix image error

--- ClothingOnlineWeb/Startup.cs
+++ ClothingOnlineWeb/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,7 +34,11 @@ namespace ClothingOnlineWeb
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseFileServer(
+        new FileServerOptions()
+        {
+            FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot"))
+        });
             app.UseRouting();
             //kich hoat session, su dung cho website
             app.UseSession();
