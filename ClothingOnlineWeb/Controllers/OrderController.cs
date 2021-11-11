d9aa6ea4ac2c01912d6c37239b18f15c32ceefdd
Dao Viet Anh <vietanh.dao99@gmail.com>
2021-11-12 03:47:56 +0700

    view cart and first check out

---
+++ ClothingOnlineWeb/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using ClothingOnlineWeb.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingOnlineWeb.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddToOrder()
+        {
+            var session = HttpContext.Session;
+            string jsoncart = session.GetString("cart");
+            List<Cart> carts = new List<Cart>();
+            if (jsoncart != null)
+            {
+                carts = JsonConvert.DeserializeObject<List<Cart>>(jsoncart);
+            }
+            return View(carts);
+        }
+    }
+}
