f2ae2c82277eed4725e6910787ddae80c168204b
namthhe130880 <93973312+namthhe130880@users.noreply.github.com>
2021-11-12 22:02:52 +0700

    AdminController

---
+++ ClothingOnlineWeb/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using ClothingOnlineWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothingOnlineWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+        public IActionResult Accounts()
+        {
+            ProjectPRN211Context context = new ProjectPRN211Context();
+            return View("tables",context.Accounts.ToList());
+        }
+        public IActionResult Product()
+        {
+            ProjectPRN211Context context = new ProjectPRN211Context();
+            return View(context.Products.ToList());
+        }
+    }
+}
