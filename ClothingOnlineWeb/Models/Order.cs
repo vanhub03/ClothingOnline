c3184558f0bf0f95422a20571c322170cd0156d1
viethqhd <52351229+viethqhd@users.noreply.github.com>
2021-11-14 20:18:39 +0700

    ValidateOrderInfo

--- ClothingOnlineWeb/Models/Order.cs
+++ ClothingOnlineWeb/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -14,8 +15,16 @@ namespace ClothingOnlineWeb.Models
 
         public int Orderid { get; set; }
         public int Accountid { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Username length must be between 2 and 50", MinimumLength = 2)]
         public string Customername { get; set; }
+
+        [Required]
+        [RegularExpression(@"/^\d{10}$/i", ErrorMessage = "Phone must be 10 digits")]
         public string Phone { get; set; }
+
+        [Required]
         public string Address { get; set; }
         public string Totalprice { get; set; }
         public int Status { get; set; }
